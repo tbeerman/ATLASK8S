cb1958f041a2d1a6324dcce5b2494c919f3df58f
Thomas Beermann <thomas.beermann@cern.ch>
2019-12-02 14:08:09 +0100

    add script to use pvc with prometheus

---
+++ add_prometheus_pvc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CLUSTER=atlasrucioint3
+mkdir -p ${HOME}/ws/helm_home_${CLUSTER}
+HELM_HOME="${HOME}/ws/helm_home_${CLUSTER}"
+HELM_TLS_ENABLE="true"
+TILLER_NAMESPACE="magnum-tiller"
+
+kubectl -n magnum-tiller get secret helm-client-secret -o jsonpath='{.data.ca\.pem}' | base64 --decode > "${HELM_HOME}/ca.pem"
+kubectl -n magnum-tiller get secret helm-client-secret -o jsonpath='{.data.key\.pem}' | base64 --decode > "${HELM_HOME}/key.pem"
+kubectl -n magnum-tiller get secret helm-client-secret -o jsonpath='{.data.cert\.pem}' | base64 --decode > "${HELM_HOME}/cert.pem"
+
+helm ls
+helm init -c
+helm repo update
+
+helm upgrade -f prometheus-pvc-values.yaml prometheus-operator stable/prometheus-operator --namespace=kube-system --version 5.12.3 --recreate-pods
